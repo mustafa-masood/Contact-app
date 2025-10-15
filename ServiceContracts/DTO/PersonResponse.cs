132b078c2d88fb243febbc57a5bee2a01e15158b
mustafa-masood <hanisiddiqui123@gmail.com>
2025-10-15 12:01:54 +0500

    override equals method & PersonExtension class added

--- ServiceContracts/DTO/PersonResponse.cs
+++ ServiceContracts/DTO/PersonResponse.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entities;
 
 namespace ServiceContracts.DTO
 {
@@ -26,5 +27,54 @@ namespace ServiceContracts.DTO
         public double? Age { get; set; }
 
 
+        /// <summary>
+        /// compares the current object data with the parameter object
+        /// </summary>
+        /// <param name="obj">The PersonResponse object to compare</param>
+        /// <returns>True or False indication whether all person details are matched with the specified parameter object</returns>
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            if (obj.GetType() != typeof(PersonResponse)) return false;
+
+            PersonResponse person_to_compare = (PersonResponse)obj;
+            return PersonID == person_to_compare.PersonID
+                && PersonName == person_to_compare.PersonName
+                && Email == person_to_compare.Email
+                && DateOfBirth == person_to_compare.DateOfBirth
+                && Gender == person_to_compare.Gender
+                && CountryID == person_to_compare.CountryID
+                && Address == person_to_compare.Address
+                && RecieveNewsLetters == person_to_compare.RecieveNewsLetters;
+
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
-}
+        public static class PersonExtensions
+        {
+            /// <summary>
+            /// Converts a Person domain model object to a PersonResponse DTO object.
+            /// </summary>
+            /// <param name="person">The Person object to convert.</param>
+            /// <returns>Returns the converted PersonResponse object</returns>
+            public static PersonResponse ToPersonResponse(this Person person)
+            {
+                return new PersonResponse()
+                {
+                    PersonID = person.PersonID,
+                    PersonName = person.PersonName,
+                    Email = person.Email,
+                    DateOfBirth = person.DateOfBirth,
+                    Gender = person.Gender,
+                    RecieveNewsLetters = person.RecieveNewsLetters,
+                    CountryID = person.CountryID,
+                    Address = person.Address,
+                    Age = person.DateOfBirth.HasValue ? Math.Round((DateTime.Now - person.DateOfBirth.Value).TotalDays / 365.25, 1) : null
+                };
+            }
+        }
+    }
